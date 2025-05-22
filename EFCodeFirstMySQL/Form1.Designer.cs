4491d8f624d84e065ee2b1db5e28a4c370b6fe65
gmand <gmand@DESKTOP-2KO8PEB>
2025-05-22 16:42:53 -0400

    Componentes formulário

--- EFCodeFirstMySQL/Form1.Designer.cs
+++ EFCodeFirstMySQL/Form1.Designer.cs
@@ -28,18 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnSalvar = new Button();
+            label1 = new Label();
+            txtNome = new TextBox();
+            txtEmail = new TextBox();
+            label2 = new Label();
+            chkAtivo = new CheckBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            btnSalvar.Location = new Point(351, 63);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.Size = new Size(75, 23);
+            btnSalvar.TabIndex = 0;
+            btnSalvar.Text = "Salvar";
+            btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(43, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nome:";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(133, 34);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(196, 23);
+            txtNome.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(133, 63);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(196, 23);
+            txtEmail.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(43, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Email:";
+            // 
+            // chkAtivo
+            // 
+            chkAtivo.AutoSize = true;
+            chkAtivo.Location = new Point(136, 105);
+            chkAtivo.Name = "chkAtivo";
+            chkAtivo.Size = new Size(54, 19);
+            chkAtivo.TabIndex = 3;
+            chkAtivo.Text = "Ativo";
+            chkAtivo.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(57, 153);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(369, 112);
+            dataGridView1.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(471, 277);
+            ClientSize = new Size(471, 292);
+            Controls.Add(dataGridView1);
+            Controls.Add(chkAtivo);
+            Controls.Add(txtEmail);
+            Controls.Add(txtNome);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnSalvar);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnSalvar;
+        private Label label1;
+        private TextBox txtNome;
+        private TextBox txtEmail;
+        private Label label2;
+        private CheckBox chkAtivo;
+        private DataGridView dataGridView1;
     }
 }
