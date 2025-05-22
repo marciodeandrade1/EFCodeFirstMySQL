635b5dbce6cefdfa9d446b83059659ce998f4235
gmand <gmand@DESKTOP-2KO8PEB>
2025-05-22 16:26:56 -0400

    Instalação de dependências e criação Model Cliete.cs

---
+++ EFCodeFirstMySQL/Model/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCodeFirstMySQL.Model
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+    }
+}
