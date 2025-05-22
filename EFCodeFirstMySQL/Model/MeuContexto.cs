d75d4beb4cc9e1a91c0c8766f0f1e405b9aaeaad
gmand <gmand@DESKTOP-2KO8PEB>
2025-05-22 16:34:43 -0400

    Contexto e App.config

---
+++ EFCodeFirstMySQL/Model/MeuContexto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstMySQL.Model
+{
+    public class MeuContexto : DbContext
+    {
+        public MeuContexto() : base ("name=MeuContexto")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<MeuContexto>());
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
